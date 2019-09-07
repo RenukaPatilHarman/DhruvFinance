021bf886ac9adc9a05f8ae745ab90ad8962d3f1d
Mahesh Shelke <mahesh.shelke08@gmail.com>
2019-09-08 01:04:32 +0530

    Create Spring Boot Project
    Create folder frontend and backend

---
+++ back_end/Dhruv/src/main/java/com/dhruv/server/DhruvApplication.java
@@ -0,0 +1,13 @@
+package com.dhruv.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhruvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhruvApplication.class, args);
+	}
+
+}
